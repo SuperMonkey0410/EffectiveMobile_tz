3c64bb3e455d57e283cb5e84657bf0459e388198
SuperMonkey0410 <145975354+SuperMonkey0410@users.noreply.github.com>
2024-10-08 16:34:45 +0300

    init commit

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = sessionmaker(engine, expire_on_commit=False,)
+
+class Base (DeclarativeBase):
+    pass
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='Склад'
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
