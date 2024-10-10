c7e6c54dea0ca0b968e7f54b9386585a135f1c73
Михаил <145975354+SuperMonkey0410@users.noreply.github.com>
2024-10-10 16:18:36 +0300

    change main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from app.database import engine, Base
+from app.routers import products, orders
 app = FastAPI(title='Склад')
 
 
@@ -8,3 +9,5 @@ async def root():
     return {"message": "Hello World"}
 
 
+app.include_router(products.router)
+app.include_router(orders.router)
